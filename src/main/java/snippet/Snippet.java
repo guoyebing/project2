bdcca19e557a89a9aa5cccac585cd27470de06ca
liujinping <946436385@qq.com>
2018-10-30 15:57:04 +0800

    sss

--- src/main/java/snippet/Snippet.java
+++
@@ -1,7 +0,0 @@
-package snippet;
-
-public class Snippet {
-	created_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP comment '数据创建时间',
-		updated_at datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP comment '数据更新时间',
-}
-
